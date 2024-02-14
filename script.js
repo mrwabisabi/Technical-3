1f3da70b21a9c6777013de8f02e06c31fe804313
mrwabisabi <137009699+mrwabisabi@users.noreply.github.com>
2024-02-15 00:58:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let result = '';
+
+function appendToResult(value) {
+    result += value;
+    document.getElementById('result').value = result;
+}
+
+function clearResult() {
+    result = '';
+    document.getElementById('result').value = result;
+}
+
+function operate(operator) {
+    result += operator;
+    document.getElementById('result').value = result;
+}
+
+function calculate() {
+    try {
+        result = eval(result);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
